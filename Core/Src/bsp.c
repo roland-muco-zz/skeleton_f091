3d214271ef080d479d65fe410541d36d0d0a68ef
roland van straten <roland@muco-group.com>
2020-11-20 23:15:34 +0100

    Add test for hardware CRC use as working template

--- Core/Src/bsp.c
+++ Core/Src/bsp.c
@@ -8,10 +8,14 @@
 
 #include <stdio.h>
 #include "stm32f0xx_hal.h"
+#include "bsp.h"
+#include "crc.h"
 #include "i2c.h"
 #include "usart.h"
 #include "main.h"
 
+
+void bsp_hwcrc_reinit(crc_modes_t selector);
 
 /* implementation */
 
@@ -121,3 +125,146 @@ void bsp_welcome(void)
 	/* famous quote from Emerson, Lake & Palmer concert */
 	printf("Welcome friends to the show that never ends...\r\n");
 }
+
+
+#if(1)
+/*******************
+ * CRC HARDWARE
+ *
+ * Even though all STM32's have a hardware CRC provision, it seems to be not very popular. Why spend
+ * so much space in flash or computation time to crc ehile there is a number cruncher in the micro-controller
+ *
+ * https://reveng.sourceforge.io/crc-catalogue/17plus.htm#crc.cat.crc-32c
+ * https://www.lammertbies.nl/comm/info/crc-calculation
+ *
+ *******************/
+
+/**
+ * \brief	test
+ *
+ */
+void bsp_hwcrc_test(void)
+{
+	uint8_t data[9] = {0x31,0x32,0x33,0x34,0x35,0x36,0x37,0x38,0x39};
+
+	printf("STM32 Hardware CRC Test\r\n");
+
+	uint16_t chcksm = 0;
+	for(int i = 0; i<9; i++)
+	{
+		chcksm = (chcksm + data[i]) % 256;
+	}
+	printf("Checksum MODULO 256 %x\r\n", chcksm);
+
+	bsp_hwcrc_reinit(CRC16_XMODEM);
+	printf("CRC16 XMODEM %lx\r\n", HAL_CRC_Calculate(&hcrc, (uint32_t*)&data[0], 9) );
+
+	bsp_hwcrc_reinit(CRC16_MODBUS);
+	printf("CRC16 MODBUS %lx\r\n", HAL_CRC_Calculate(&hcrc, (uint32_t*)&data[0], 9) );
+}
+
+
+/**
+ * \brief	reconfigure the CRC peripheral
+ *
+ */
+void bsp_hwcrc_reinit(crc_modes_t selector)
+{
+	__HAL_RCC_CRC_CLK_ENABLE();
+
+	hcrc.Instance = CRC;
+
+	switch(selector)
+	{
+	case 0:
+	    /* width=16
+	     * poly=0x1021
+	     * init=0x0000
+	     * refin=false
+	     * refout=false
+	     * xorout=0x0000
+	     * check=0x31c3
+	     * residue=0x0000
+	     * name="CRC-16/XMODEM"
+	     */
+
+/* width */
+//			hcrc.InputDataFormat = CRC_INPUTDATA_FORMAT_UNDEFINED;
+			hcrc.InputDataFormat = CRC_INPUTDATA_FORMAT_BYTES;
+//			hcrc.InputDataFormat = CRC_INPUTDATA_FORMAT_HALFWORDS;
+//			hcrc.InputDataFormat = CRC_INPUTDATA_FORMAT_WORDS;
+
+/* poly - default polynomial 0x04C11DB7 */
+//			hcrc.Init.CRCLength = CRC_POLYLENGTH_32B;
+			hcrc.Init.CRCLength = CRC_POLYLENGTH_16B;
+//			hcrc.Init.CRCLength = CRC_POLYLENGTH_8B;
+//			hcrc.Init.CRCLength = CRC_POLYLENGTH_7B;
+
+			hcrc.Init.GeneratingPolynomial = 0x1021;
+	    	hcrc.Init.DefaultPolynomialUse = DEFAULT_POLYNOMIAL_DISABLE;
+//			hcrc.Init.DefaultPolynomialUse = DEFAULT_POLYNOMIAL_ENABLE;
+
+/* init */	hcrc.Init.DefaultInitValueUse = DEFAULT_INIT_VALUE_DISABLE;
+//			hcrc.Init.DefaultInitValueUse = DEFAULT_INIT_VALUE_ENABLE;
+	    	hcrc.Init.InitValue = 0;
+
+/* refin */	hcrc.Init.InputDataInversionMode = CRC_INPUTDATA_INVERSION_NONE;
+//			hcrc.Init.InputDataInversionMode = CRC_INPUTDATA_INVERSION_BYTE;
+//			hcrc.Init.InputDataInversionMode = CRC_INPUTDATA_INVERSION_HALFWORD;
+//			hcrc.Init.InputDataInversionMode = CRC_INPUTDATA_INVERSION_WORD;
+
+/* refout */hcrc.Init.OutputDataInversionMode = CRC_OUTPUTDATA_INVERSION_DISABLE;
+//			hcrc.Init.OutputDataInversionMode = CRC_OUTPUTDATA_INVERSION_ENABLE;
+
+	break;
+
+	case 1:
+		/* width=16
+		 * poly=0x8005
+		 * init=0xffff
+		 * refin=true
+		 * refout=true
+		 * xorout=0x0000
+		 * check=0x4b37
+		 * residue=0x0000
+		 * name="CRC-16/MODBUS"
+		 */
+
+/* width */
+//			hcrc.InputDataFormat = CRC_INPUTDATA_FORMAT_UNDEFINED;
+			hcrc.InputDataFormat = CRC_INPUTDATA_FORMAT_BYTES;
+//			hcrc.InputDataFormat = CRC_INPUTDATA_FORMAT_HALFWORDS;
+//			hcrc.InputDataFormat = CRC_INPUTDATA_FORMAT_WORDS;
+
+/* poly - default polynomial 0x04C11DB7 */
+//			hcrc.Init.CRCLength = CRC_POLYLENGTH_32B;
+			hcrc.Init.CRCLength = CRC_POLYLENGTH_16B;
+//			hcrc.Init.CRCLength = CRC_POLYLENGTH_8B;
+//			hcrc.Init.CRCLength = CRC_POLYLENGTH_7B;
+
+			hcrc.Init.GeneratingPolynomial = 0x8005;
+			hcrc.Init.DefaultPolynomialUse = DEFAULT_POLYNOMIAL_DISABLE;
+//			hcrc.Init.DefaultPolynomialUse = DEFAULT_POLYNOMIAL_ENABLE;
+
+/* init */	hcrc.Init.DefaultInitValueUse = DEFAULT_INIT_VALUE_DISABLE;
+//			hcrc.Init.DefaultInitValueUse = DEFAULT_INIT_VALUE_ENABLE;
+			hcrc.Init.InitValue = 0xFFFF;
+
+/* refin */
+//			hcrc.Init.InputDataInversionMode = CRC_INPUTDATA_INVERSION_NONE;
+			hcrc.Init.InputDataInversionMode = CRC_INPUTDATA_INVERSION_BYTE;
+//			hcrc.Init.InputDataInversionMode = CRC_INPUTDATA_INVERSION_HALFWORD;
+//			hcrc.Init.InputDataInversionMode = CRC_INPUTDATA_INVERSION_WORD;
+
+/* refout */
+//			hcrc.Init.OutputDataInversionMode = CRC_OUTPUTDATA_INVERSION_DISABLE;
+			hcrc.Init.OutputDataInversionMode = CRC_OUTPUTDATA_INVERSION_ENABLE;
+	break;
+	}
+
+    if (HAL_CRC_Init(&hcrc) != HAL_OK)
+    {
+        Error_Handler();
+    }
+}
+#endif
