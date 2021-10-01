161b3ce0f2b448c9172d72e2064f87da8f05bf1a
roland van straten <roland@muco-group.com>
2021-10-01 13:08:02 +0200

    Add USART5

--- Core/Src/usart.c
+++ Core/Src/usart.c
@@ -8,7 +8,27 @@
 
 
 UART_HandleTypeDef huart2;
+UART_HandleTypeDef huart5;
 
+/* USART5 init function */
+void MX_USART5_UART_Init(void)
+{
+  huart5.Instance = USART5;
+  huart5.Init.BaudRate = 115200;
+  huart5.Init.WordLength = UART_WORDLENGTH_8B;
+  huart5.Init.StopBits = UART_STOPBITS_1;
+  huart5.Init.Parity = UART_PARITY_NONE;
+  huart5.Init.Mode = UART_MODE_TX_RX;
+  huart5.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart5.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart5.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart5.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart5) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+}
 
 /* USART2 init function */
 void MX_USART2_UART_Init(void)
@@ -34,6 +54,7 @@ void HAL_UART_MspInit(UART_HandleTypeDef* uartHandle)
 {
 
   GPIO_InitTypeDef GPIO_InitStruct = {0};
+
   if(uartHandle->Instance==USART2)
   {
     __HAL_RCC_USART2_CLK_ENABLE();
@@ -49,6 +70,23 @@ void HAL_UART_MspInit(UART_HandleTypeDef* uartHandle)
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
     GPIO_InitStruct.Alternate = GPIO_AF1_USART2;
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  }
+  else if(uartHandle->Instance==USART5)
+  {
+    __HAL_RCC_USART5_CLK_ENABLE();
+
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+    /**USART5 GPIO Configuration
+    PB3     ------> USART5_TX
+    PB4     ------> USART5_RX
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_3|GPIO_PIN_4;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF4_USART5;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
    }
 }
@@ -67,4 +105,16 @@ void HAL_UART_MspDeInit(UART_HandleTypeDef* uartHandle)
     HAL_GPIO_DeInit(GPIOA, GPIO_PIN_2|GPIO_PIN_3);
 
   }
+  else if(uartHandle->Instance==USART5)
+  {
+    __HAL_RCC_USART5_CLK_DISABLE();
+
+    /**USART5 GPIO Configuration
+    PB3     ------> USART5_TX
+    PB4     ------> USART5_RX
+    */
+    HAL_GPIO_DeInit(GPIOB, GPIO_PIN_3|GPIO_PIN_4);
+
+  }
+
 }
